5867790c626a6d04cf7dc200b7d36e116bebb982
lilahaj <fedor.pilotov@gmail.com>
2026-01-28 21:38:47 +0200

    fix

--- unit_testing/main.js
+++ unit_testing/main.js
@@ -1,11 +1,22 @@
-try {
-  const delegated = require("./Unit_Test/main.js");
-  module.exports = delegated;
+const mylib = require("./Unit_Test/mylib");
 
-  if (require.main === module && typeof delegated.main === "function") {
-    delegated.main();
+function main() {
+  console.log("Demo of mylib:");
+  console.log("2 + 3 =", mylib.add(2, 3));
+  console.log("10 - 4 =", mylib.sub(10, 4));
+  console.log("6 * 7 =", mylib.mul(6, 7));
+  console.log("8 / 2 =", mylib.div(8, 2));
+
+  try {
+    console.log("Attempting division by zero:");
+    console.log("8 / 0 =", mylib.div(8, 0));
+  } catch (err) {
+    console.error("Caught error in main:", err.message);
   }
-} catch (err) {
-  console.error("Failed to load Unit_Test/main.js:", err.message);
-  throw err;
 }
+
+if (require.main === module) {
+  main();
+}
+
+module.exports = { main };
